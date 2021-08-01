650ec2390b0154c32c659099bc01bb21be5a7c77
Toptre <Sankar.blagodirov@gmail.com>
2021-08-01 21:39:11 +0200

    aa

--- BankApp/Controllers/TransactionController.cs
+++ BankApp/Controllers/TransactionController.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BankApp.Data;
 using BankApp.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BankApp.Controllers
 {
-    public class TransactionController : Controller
+    public class TransactionController : BaseController
     {
+        public TransactionController ()
         public IActionResult Index()
         {
             return View();
@@ -27,10 +29,33 @@ namespace BankApp.Controllers
         public IActionResult NewDeposit(NewDepositViewModel model)
         {
 
+            if (ModelState.IsValid)
+            {
+                var dbTrans = new Transaction();
+                _transaction.AddTrans(dbTrans);
+                dbTrans.AccountId = viewModel.AccountId;
+                dbTrans.Date = DateTime.Now;
+                dbTrans.Type = "Credit";
+                dbTrans.Operation = "Credit in Cash";
+                dbTrans.Amount = viewModel.Amount;
 
+
+                var dbAcc = _account.GetAllAccount().First(r => r.AccountId == viewModel.AccountId);
+                var balance = dbAcc.Balance + viewModel.Amount;
+
+                dbTrans.Balance = balance;
+                dbAcc.Balance = balance;
+
+                _transaction.Save();
+                return RedirectToAction("New");
+
+
+            }
 
             return View();
         }
+
+
         
     }
 }
--- BankApp/Services/ITransactionRepository.cs
+++ BankApp/Services/ITransactionRepository.cs
@@ -9,19 +9,31 @@ namespace BankApp.Services
     public interface ITransactionRepository
     {
         IQueryable<Transaction> GetAllTransactions();
+        public void AddTransaction(Transaction dbTransaction);
+        public void Save();
     }
 
     class TransactionsRepository:ITransactionRepository
     {
-        protected readonly BankAppDataContext _dbcontext;
+        protected readonly BankAppDataContext _dbContext;
         public TransactionsRepository(BankAppDataContext dbcontext)
         {
-            _dbcontext = dbcontext;
+            _dbContext = dbcontext;
+        }
+
+        public void AddTransaction(Transaction dbTransaction)
+        {
+            _dbContext.Transactions.Add(dbTransaction);
         }
 
         public IQueryable<Transaction> GetAllTransactions()
         {
-            return _dbcontext.Transactions;
+            return _dbContext.Transactions;
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
         }
     }
 }
